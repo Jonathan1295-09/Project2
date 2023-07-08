5c7006dfac88b3d7d495bc803e9151ebb1ecdb0c
Jonathan1295-09 <jcasasola955@gmail.com>
2023-07-08 15:52:01 -0500

    controllers and server.js

---
+++ controllers/trackLog.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const TrackLog = require('../models/trackLog');
+
+const router = express.Router();
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const trackLogRouter = require('./controllers/trackLog')
 
 // express application
 const app = express()
