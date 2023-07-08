1f8e3a2bcaf41cedcae38f04d8d4dcb6148daf8a
Jonathan1295-09 <jcasasola955@gmail.com>
2023-07-08 14:13:25 -0500

    update fix to seed.js ();

--- models/seed.js
+++ models/seed.js
@@ -4,8 +4,8 @@ const mongoose = require('./connection');
 mongoose.connection.on('open', async () => {
     await TrackLog.deleteMany();
     const startTrackLogs = [
-        {track:"Road America", lapTime: "2:31.6", weather: "sunny", tire:"RT660", model:"370z"}
+        {track:"Road America", lapTime: "2:31.6", weather: "sunny", tire:"RT660", model:"370z"}// had issue here with laptime i passed as a number but guess it need to be a string in order for it to work
     ];
     await TrackLog.create(startTrackLogs);
-    mongoose.connection.close
+    mongoose.connection.close();
 });
