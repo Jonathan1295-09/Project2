ee783f7c8b05a550229b824f3feeb458850afc16
Jonathan1295-09 <jcasasola955@gmail.com>
2023-07-10 23:03:56 -0500

    got the index to work

--- controllers/trackLog.js
+++ controllers/trackLog.js
@@ -3,8 +3,9 @@ const TrackLog = require('../models/trackLog');
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    res.render("tracklogs/index.ejs")
+router.get("/", async (req,res) => {
+    const allTracklog = await TrackLog.find({})
+    res.render("tracklogs/index.ejs", {tracklogs: allTracklog})
 })
 
 module.exports = router
