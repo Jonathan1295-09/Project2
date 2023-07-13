19492694c7b8aa12e1f6ead9024606e146cdaf6c
Jonathan1295-09 <jcasasola955@gmail.com>
2023-07-13 18:20:06 -0500

    created users.js and login.ejs

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-
 const router = express.Router();
+const TrackLog = require('../models/trackLog');
+const User = require('../models/user');
 
-router.get('/login', (req,res) => {
-    req.session.destroy(err => {
-        res.redirect('/')
-    })
-})
+
+router.get('/', (req,res) => {
+   res.render('users/login.ejs');
+});
+
+router.post('/login', async (req,res) => {
+    const user = await User.findOne({ username: req.body.username });
+    if (user) {
+        res.send('User Does Not Exist')
+    }else{
+        const passmatches = bcrypt.compareSync(req.body.username, user.password)
+        if (passmatches){
+            req.session.username = req.body.username;
+            req.session.loggedIn = true;
+            res.redirect('tracklogs/index.ejs')
+        }
+    }
+});
+
 module.exports = router;
